db7147b66258691058247b193a8c80187702462d
Anastasiia Solop <35258279+anastasiya1155@users.noreply.github.com>
2024-12-06 16:26:18 +0000

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 console.log("Hello world!");
 const args = process.argv.slice(2);
 console.log("args", args);
-fs.readFileSync(args[0]);
+fs.readFileSync(args[0] + "/qodana.sarif.json");
 console.log("finished reading");
 fs.writeFileSync("./new_file.java", "System.out.println(\"Hello\")");
 console.log("finished writing");
